4651b907a1c16c8d5e2f425943b2eb39dd95837d
O. Qudah <69857856+BasicPixel@users.noreply.github.com>
2022-01-20 11:08:00 +0200

    Setup for Heroku deployment

--- commerce/settings.py
+++ commerce/settings.py
@@ -125,3 +125,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 STATIC_URL = '/static/'
 LOGIN_URL = '/login'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Configure Django App for Heroku.
+import django_on_heroku
+django_on_heroku.settings(locals())
